138c3dde426cea8814b1bdb00ef753fca5f56ef3
Jason Klein <jklein.64@verizon.net>
2022-02-17 15:16:16 -0500

    Add testing code

--- hierarchy-generation.py
+++ hierarchy-generation.py
@@ -9,7 +9,13 @@ def main():
     parser.add_argument("checkpoints", help="Segmentation model checkpoints file")
     args = parser.parse_args()
 
-    print(args)
+    # build the model from a config file and a checkpoint file
+    # Figure out how to use CUDA later, if it's even possible on M1
+    model = init_segmentor(args.config, args.checkpoints, device="cpu")
+
+    result = inference_segmentor(model, args.image)
+
+    print(result)
 
 if __name__ == "__main__":
     main()
