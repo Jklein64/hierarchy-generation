1c23d828c1c72e3022ac9a7d560c739e1028dcbc
Jason Klein <jklein.64@verizon.net>
2022-02-18 14:55:38 -0500

    Refactor segmentation into a method

--- hierarchy-generation.py
+++ hierarchy-generation.py
@@ -11,25 +11,32 @@ def main():
     parser.add_argument("checkpoints", help="Segmentation model checkpoints file")
     args = parser.parse_args()
 
+    segments = segment_image(np.array(Image.open(args.image)), args.config, args.checkpoints)
+    for i, segment in enumerate(segments):
+        Image.fromarray(segment).save(f"output/segment-{i}.png")
+
+
+def segment_image(image, config, checkpoints):
+    """Segment the given image with the segmentation model described by config and checkpoints.  Expects image to be an array and the others to be file paths.  Returns a list of image-sized arrays, one for each segment.
+    """
     # build the model from a config file and a checkpoint file
     # Figure out how to use CUDA later, if it's even possible on M1
-    model = init_segmentor(args.config, args.checkpoints, device="cpu")
-
+    model = init_segmentor(config, checkpoints, device="cpu")
     # result has shape 1 x H x W, storing an integer label
-    result = inference_segmentor(model, args.image)[0]
-
-    # get original image, which has shape H x W x 3
-    image = np.array(Image.open(args.image))
+    result = inference_segmentor(model, image)[0]
     # add a new axis to the image for alpha. Roll is needed to add to the end
     image = np.roll(np.insert(image, 0, 255, axis=-1), -1, axis=-1)
-
-    for i, label in enumerate(np.unique(result)):
+    
+    segments = []
+    for label in np.unique(result):
         # make new image with same dimensions
         output = np.zeros_like(image)
         # set pixels to result where label is label
         np.copyto(output, image, where=np.expand_dims(result == label, axis=-1))
-        # save as "segment-i.png"
-        Image.fromarray(output).save(f"output/segment-{i}.png")
+        # collect into list of segments
+        segments.append(output)
+
+    return segments        
 
 if __name__ == "__main__":
     main()
