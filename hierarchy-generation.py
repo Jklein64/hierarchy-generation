3bf991450e4ec3fcf8dcd5c8639121ee5148ce99
Jason Klein <jklein.64@verizon.net>
2022-02-18 13:53:39 -0500

    Import image and get mask

--- hierarchy-generation.py
+++ hierarchy-generation.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 
 from mmseg.apis import inference_segmentor, init_segmentor
+from PIL import Image
+import numpy as np
 
 def main():
     parser = ArgumentParser(description="Generate a segmentation hierarchy from an image.")
@@ -13,9 +15,19 @@ def main():
     # Figure out how to use CUDA later, if it's even possible on M1
     model = init_segmentor(args.config, args.checkpoints, device="cpu")
 
-    result = inference_segmentor(model, args.image)
+    # result has shape 1 x H x W, storing an integer label
+    result = inference_segmentor(model, args.image)[0]
 
-    print(result)
+    # get original image, which has shape H x W x 3
+    image = np.array(Image.open(args.image))
+
+    for i, label in enumerate(np.unique(result)):
+        # make new image with same dimensions
+        output = np.zeros_like(image)
+        # set pixels to result where label is label
+        np.copyto(output, image, where=np.expand_dims(result == label, axis=-1))
+        # save as "segment-i.png"
+        ...
 
 if __name__ == "__main__":
     main()
