ab6ec88b88b3cc66da6277bba5d9e0c44a8db3e0
Jason Klein <jklein.64@verizon.net>
2022-03-21 11:23:40 -0400

    Remove SenFormer

--- hierarchy-generation.py
+++ hierarchy-generation.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 
-from mmseg.apis import inference_segmentor# init_segmentor
-from SenFormer.demo.image_demo import init_segmentor
+from mmseg.apis import inference_segmentor, init_segmentor
 from scipy.ndimage import label
 from PIL import Image
 import numpy as np
@@ -36,7 +35,7 @@ def segment_image(image, config, checkpoints):
     """Segment the given image with the segmentation model described by config and checkpoints.  Expects image to be an array and the others to be file paths.  Returns a list of image-sized arrays, one for each segment."""
     # build the model from a config file and a checkpoint file
     # Figure out how to use CUDA later, if it's even possible on M1
-    model = init_segmentor(config, checkpoints, device="cpu", palette="cityscapes", classes="cityscapes")
+    model = init_segmentor(config, checkpoints, device="cpu")
     # result has shape 1 x H x W, storing an integer label
     result = inference_segmentor(model, image[:,:,0:3])[0]
     # add a new axis to the image for alpha. Roll is needed to add to the end
