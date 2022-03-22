c2c471574932b381b2b0074b5cd39289e540d1aa
Jason Klein <jklein.64@verizon.net>
2022-03-22 13:41:27 -0400

    Make scripts valid python modules

--- superpixel_segmentation.py
+++ superpixel_segmentation.py
@@ -2,6 +2,7 @@ from skimage.segmentation import slic, mark_boundaries
 from PIL import Image
 import numpy as np
 
+# slic() expects images to be floating point and 3-channel
 image = np.array(Image.open("./output/segment-1.png"))[...,0:3] / 255
 # segments is an array which maps each pixel location to a segment label
 segments = slic(image, 200, start_label=0, multichannel=True)
