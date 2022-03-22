fac358f2b83816aabc08b72b8af1e8d1519b461a
Jason Klein <jklein.64@verizon.net>
2022-03-22 13:38:33 -0400

    Create superpixel script

---
+++ superpixel-segmentation.py
@@ -0,0 +1,9 @@
+from skimage.segmentation import slic, mark_boundaries
+from PIL import Image
+import numpy as np
+
+image = np.array(Image.open("./output/segment-1.png"))[...,0:3] / 255
+# segments is an array which maps each pixel location to a segment label
+segments = slic(image, 200, start_label=0, multichannel=True)
+Image.fromarray((mark_boundaries(image, segments) * 255).astype(np.uint8)).show()
+pass
