2b0a5b26b44ddb4dbc8e6f0ce77f04d04c51e4f0
Jason Klein <jklein.64@verizon.net>
2022-03-27 21:43:46 -0400

    Successfully merge connected components

--- superpixel_segmentation.py
+++ superpixel_segmentation.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 from skimage.segmentation import slic
 from PIL import Image
-import ot, numpy as np
+
+import numpy as np
+
 
 def main():
-    # given original, which is a segment, possibly with opacity
+    # given original, which is a segment, possibly with opacity;
+    # transparent if within bounding box but outside segment
     original = np.array(Image.open("./output/segment-1.png"))
     # slic() expects images to be floating point and 3-channel
     image = original[..., 0:3] / 255
@@ -17,7 +20,7 @@ def main():
     opaque = [label for (label, p) in enumerate(superpixels) if np.any(p[..., -1] != 0)]
     superpixels = [superpixels[label] for label in opaque]
     # create the list of pixel locations corresponding to each visible superpixel
-    pixel_indices = [np.column_stack(np.where(labels == label)) for label in opaque]
+    pixel_indices = [np.where(labels == label) for label in opaque]
 
     # create (initially) lower-triangular distances matrix
     distances = np.zeros((len(superpixels), len(superpixels)))
@@ -28,12 +31,42 @@ def main():
     # make distances an actual adjacency matrix
     distances = distances + np.transpose(distances)
 
+    # merge connected regions
+    regions = []
+    for component in connected_within_threshold(distances, 0.02):
+        # map superpixel index to the superpixel's pixel locations
+        indices = [pixel_indices[i] for i in component]
+        # concatenate; must be manual since np.where is a tuple
+        concatenated_rows = []
+        concatenated_cols = []
+        for rows, cols in indices:
+            concatenated_rows.append(rows)
+            concatenated_cols.append(cols)
+        # tuple allows for slicing of numpy arrays
+        regions.append((
+            np.concatenate(concatenated_rows),
+            np.concatenate(concatenated_cols)))
+    for region in regions:
+        image = np.zeros_like(original)
+        image[region] = original[region]
+
     
 # superpixels_to_join = np.column_stack(distance_indices)[np.where(distances[distance_indices] < 0.01)]
     print(distances)
 
+def connected_within_threshold(weighed: np.ndarray, delta: float = 0.01):
+    """Calculate the sets of connected components of a weighed undirected graph whose weights are within a threshold delta."""
+    from scipy.sparse.csgraph import connected_components
+    # labels maps index of node to a label for the group
+    n, labels = connected_components(weighed < delta, directed=False)
+    # need the first element of np.nonzero since it wraps itself in a tuple
+    return [np.nonzero(labels == label)[0] for label in range(n)]
+
+
+
 def wasserstein_image_distance(pixels_1: np.ndarray, pixels_2: np.ndarray) -> float:
     """Compute the Wasserstein or Earth Mover's distance between the given sets of integer-valued 8-bit pixels."""
+    import ot
     # ignore pixels from the superpixels which are outside of the segment,
     # but where the superpixel still has some pixels inside the segment
     pixels_1 = pixels_1[pixels_1[..., -1] != 0]
