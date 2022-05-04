5792a4853478897349f3604b7f9306f6b5dba89d
Jason Klein <jklein.64@verizon.net>
2022-05-03 23:01:46 -0400

    Write code to create RAG

--- superpixel_segmentation.py
+++ superpixel_segmentation.py
@@ -4,6 +4,7 @@ from typing import Callable
 from argparse import ArgumentParser
 
 from scipy.sparse.csgraph import connected_components
+from scipy.ndimage import binary_dilation
 from skimage.segmentation import slic
 from PIL import Image
 
@@ -32,13 +33,16 @@ def main():
     original = np.array(Image.open(args.image))
 
     # show the original image
-    show(original, constraints=constraints)
+    # show(original, constraints=constraints)
 
     labels = np.ma.array(
         # labels maps pixel location to a superpixel label
         slic(original[..., 0:3] / 255, 200, start_label=0),
         # don't mask anything if no alpha channel, otherwise mask transparent pixels
         mask=np.shape(original)[-1] == 4 and original[..., -1] == 0)
+
+    n = neighbor_matrix(original, labels, metric=wasserstein_image_distance)
+    print(superpixel_neighbors(labels, 2))
 
     # show the initial superpixel segmentation
     show(original, regions=labels, constraints=constraints)
@@ -79,6 +83,44 @@ def main():
     pass
 
 
+def neighbor_matrix(original: np.ndarray, superpixels: np.ndarray, metric: Callable[[np.ndarray, np.ndarray], float]) -> np.ndarray:
+    """Create a weighed adjacency matrix between every pair of superpixels which neighbors each other.  Weighed region adjacency graph!  The resulting matrix is normalized so that the weights are within [0, 1], and then rescaled so that higher values correspond to higher similarity."""
+    # store list of valid superpixel labels
+    unique_labels = np.ma.compressed(np.ma.unique(superpixels))
+    # pixels is a list of pixel values for the n'th superpixel
+    pixels = [original[superpixels == label] for label in unique_labels]
+    # create n-by-n matrix to compare distances between neighbors
+    neighbors = np.zeros((len(unique_labels), len(unique_labels)))
+    # iterate through neighbors and calculate distances
+    for i in unique_labels:
+        for j in superpixel_neighbors(superpixels, i):
+            # only compute distances one way
+            if j < i:
+                neighbors[i, j] = metric(pixels[i], pixels[j])
+    # fill out the other half of the matrix
+    neighbors = neighbors + np.transpose(neighbors)
+    # normalize to be within zero and one
+    neighbors /= np.max(neighbors)
+    # bigger values for more similarity
+    neighbors[np.nonzero(neighbors)] = 1 - neighbors[np.nonzero(neighbors)]
+    # SPEED this should be a sparse matrix
+    return neighbors
+
+
+def superpixel_neighbors(superpixels: np.ndarray, i: int):
+    """Given a mapping from pixel location to superpixel label and a specific superpixel number, return the labels of all of the superpixels which neighbor the one specified."""
+    selection = superpixels == i
+    expanded = binary_dilation(selection)
+    # binary XOR to get the difference
+    difference = expanded ^ selection
+    # get label for each pixel in difference
+    labels = set()
+    for pixel in np.transpose(np.where(difference)):
+        label = superpixels[tuple(pixel)]
+        labels.add(label)
+    return labels
+
+
 def connected_within_threshold(superpixels: np.ndarray, distances: np.ndarray, delta: float = 0.01):
     """Given a mapping from pixel location to superpixel label as well as a weighted adjacency matrix, calculate the sets of connected components of a weighed undirected graph of superpixels "distances" whose weights are within a threshold delta, and return a newly labelled image."""
     # merged_labels maps index of node to a label for each newly merged group
@@ -98,7 +140,6 @@ def distances_matrix(original: np.ndarray, superpixels: np.ndarray, metric: Call
     # store list of valid superpixel labels
     unique_labels = np.ma.compressed(np.ma.unique(superpixels))
     # pixels is a list of pixel values for the n'th superpixel
-    # TODO why is pixels all zeros now?
     pixels = [original[superpixels == label] for label in unique_labels]
     # create n-by-n matrix to compare distances between n superpixels
     distances = np.zeros((len(unique_labels), len(unique_labels)))
